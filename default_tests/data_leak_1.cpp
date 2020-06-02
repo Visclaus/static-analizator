2943add7818b2b12c43bf6125aaa94899a54fa38
Visclaus <visclaus2025@gmail.com>
2020-06-02 12:01:10 +0400

    added missing tests

---
+++ default_tests/data_leak_1.cpp
@@ -0,0 +1,24 @@
+long double function_20(float var_17, unsigned long int var_10, singed long int var_32) {
+	short int var_5[35];
+	ifstream var_12.open("D://Ut");
+	signed long long int *var_3;
+	ofstream var_34.open("D://adipiscing");
+	byte var_25[45];
+	int *var_13;
+	ifstream var_7.open("D://ipsum");
+	signed long long int var_39 = 26;
+	cout<<"ea quis sint "<<var_3;
+	fchmod(var_34, var_3);
+	execute("incididunt'exercitation'dolor'");
+}
+int main() {
+	signed int *var_8;
+	signed char var_12;
+	ofstream var_14.open("D://elit");
+	float *var_23;
+	ofstream var_7.open("D://Duis");
+	int *var_3;
+	execute(var_15);
+	SetKernelObjectSecurity(var_15, var_3, var_8);
+	strerror(var_23);
+}
---
+++ default_tests/data_leak_2.cpp
@@ -0,0 +1,28 @@
+long int function_0(signed long long int *var_37, unsigned long long int *var_25, unsigned long long int *var_15, short int *var_21) {
+	singed long int var_23[15];
+	byte var_20 = 30;
+	byte var_13[5];
+	unsigned long long int *var_28;
+	unsigned int var_39[5];
+	unsigned short int var_33[15];
+	ifstream var_24.open("D://eiusmod");
+	cout<<"est velit occaecat "<<var_23;
+	mktemp(var_25);
+	long var_3 = 4950331841162702092;
+	short var_9 = 28796;
+	byte var_17 = 16532;
+	popen(var_28, var_25);
+	var_3 = var_9 + var_17;
+}
+int main() {
+	long int var_27[11];
+	ofstream var_7.open("D://veniam");
+	unsigned long long int var_22;
+	unsigned long long int var_37[23];
+	long long int var_36[1];
+	unsigned long int var_1;
+	printf(Format %s and %s, var_7, var_36);
+	execute("minim'commodo'aliquip'");
+	printf(Overflow %s and %s, var_7, var_19);
+	getenv(var_1);
+}
